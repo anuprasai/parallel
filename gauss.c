ea9bad06138f629127c4e93f850000cbe2df2841
anuprasai <anuprasai1@gmail.com>
2019-05-08 12:29:19 -0500

    j

--- gauss.c
+++ gauss.c
@@ -152,8 +152,8 @@ void print_X() {
 
 int main(int argc, char **argv) {
     parameters(argc, argv);
-    MPI_Init(NULL, NULL );
-        MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Init (&argc, &argv);	
+    MPI_Comm_rank (MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	 printf("enter the matrix size\n");
 	 scanf("%d", &N);
